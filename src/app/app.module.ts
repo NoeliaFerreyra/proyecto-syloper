7bdfbdd99fc007ca2467bd2b5c6fce7835558786
NoeliaFerreyra <noeliaeferreyra@gmail.com>
2023-07-11 17:52:26 -0300

    enviando al remoto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './componentes/nav-bar/nav-bar.component';
 import { FooterComponent } from './componentes/footer/footer.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
